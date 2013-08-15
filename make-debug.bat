8403cdcf73fae54a354c5bad12d2bdebe2bed888
Henry Slawniak <meggawatts@gmail.com>
2013-08-15 18:59:03 -0500

    Add make debug script

---
+++ make-debug.bat
@@ -0,0 +1,9 @@
+SET oldDir=%CD%
+cd %appdata%\.minecraft\resourcepacks
+SET newDir=%CD%
+cd %oldDir%
+if not exist %newDir%\Eriador-DEBUG mkdir %newDir%\Eriador-DEBUG
+xcopy /y/s %oldDir%\src\main\meta\* %newDir%\Eriador-DEBUG
+xcopy /y/s %oldDir%\src\main\pack\* %newDir%\Eriador-DEBUG\assets\minecraft
+xcopy /y/s %oldDir%\MCME-UI\src\main\pack\*.* %newDir%\Eriador-DEBUG\assets\minecraft
+pause
