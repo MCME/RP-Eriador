029e40e93fdf8c02feb7b36a6874cee57e1ce700
Henry Slawniak <meggawatts@gmail.com>
2013-10-27 18:32:20 -0500

    Clean debug directory

--- make-debug.bat
+++ make-debug.bat
@@ -3,6 +3,7 @@ cd %appdata%\.minecraft\resourcepacks
 SET newDir=%CD%
 cd %oldDir%
 if not exist %newDir%\Eriador-DEBUG mkdir %newDir%\Eriador-DEBUG
+del %newdir%\Eriador-DEBUG\*.* /s /q > debug.log
 xcopy /y/s %oldDir%\src\main\meta\* %newDir%\Eriador-DEBUG
 xcopy /y/s %oldDir%\src\main\pack\* %newDir%\Eriador-DEBUG\assets\minecraft
 xcopy /y/s %oldDir%\MCME-UI\src\main\pack\*.* %newDir%\Eriador-DEBUG\assets\minecraft
