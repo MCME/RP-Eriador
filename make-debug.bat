4b1924d9de2eedf54785abf748802bba82fea0da
Henry Slawniak <meggawatts@gmail.com>
2014-04-19 13:34:21 -0500

    Update debug script

--- make-debug.bat
+++ make-debug.bat
@@ -2,10 +2,9 @@ SET oldDir=%CD%
 cd %appdata%\.minecraft\resourcepacks
 SET newDir=%CD%
 cd %oldDir%
-if not exist %newDir%\Eriador-DEBUG mkdir %newDir%\Eriador-DEBUG
-del %newdir%\Eriador-DEBUG\*.* /s /q
-xcopy /y/s %oldDir%\src\main\meta\* %newDir%\Eriador-DEBUG
-xcopy /y/s %oldDir%\src\main\pack\* %newDir%\Eriador-DEBUG\assets\minecraft
-xcopy /y/s %oldDir%\MCME-UI\src\main\pack\*.* %newDir%\Eriador-DEBUG\assets\minecraft
-xcopy /y/s %oldDir%\MCME-Aural-Experience\src\main\pack\*.* %newDir%\Eriador-DEBUG\assets\minecraft
-pause
+if not exist %newDir%\MCME-Eriador-DEBUG mkdir %newDir%\MCME-Eriador-DEBUG
+del %newdir%\MCME-Eriador-DEBUG\*.* /s /q
+xcopy /y/s %oldDir%\src\main\meta\* %newDir%\MCME-Eriador-DEBUG
+xcopy /y/s %oldDir%\src\main\pack\* %newDir%\MCME-Eriador-DEBUG\assets\minecraft
+xcopy /y/s %oldDir%\MCME-UI\src\main\pack\*.* %newDir%\MCME-Eriador-DEBUG\assets\minecraft
+xcopy /y/s %oldDir%\MCME-Aural-Experience\src\main\pack\*.* %newDir%\MCME-Eriador-DEBUG\assets\minecraft
